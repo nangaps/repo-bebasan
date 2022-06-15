7af26e25a6a0ff39d93e6651b3d49f3636730d9c
nangaps <nangaparbat36@gmail.com>
2022-06-15 21:48:15 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>HEllO WORLD!</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus atque necessitatibus natus recusandae iure magnam hic nesciunt! Velit, ab soluta ipsum consequatur iusto magni expedita fugiat vitae assumenda laborum nemo!</p>
 </body>
 </html>
